bc8453b7bef6f81166a8e803cafa1c6956d91f1e
Chris Olwell <chrisolwell@gmail.com>
2022-11-13 19:56:18 -0600

    created jupyter nb,read in csv and created df.

---
+++ MPLSCrime.py
@@ -0,0 +1,14 @@
+# Add our dependencies.
+import csv
+import os
+import numpy as np 
+import pandas as pd
+
+# Add a variable to load a file from a path.
+file_to_load = os.path.join("Crime_Data.csv")
+#print(file_to_load)
+x = "hello world"
+print(x)
+
+    
+    
